cfc54060969c63a66519f87642c55197ea94dec5
jax-bot05 <jacqueseuan.carigma@neu.edu.ph>
2025-12-01 11:36:01 +0800

    Create Milk.java

---
+++ Milk.java
@@ -0,0 +1,11 @@
+public class Milk extends CoffeeDecorator {
+    public Milk(Coffee coffee) {
+        super(coffee);
+    }
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+    public double getCost() {
+        return coffee.getCost() + 25.00;
+    }
+}
