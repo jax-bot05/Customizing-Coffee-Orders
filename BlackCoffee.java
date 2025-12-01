81326a7dca01ec14dfe7a353ec642085ed969fde
jax-bot05 <jacqueseuan.carigma@neu.edu.ph>
2025-12-01 11:34:13 +0800

    Create  BlackCoffee.java

---
+++ BlackCoffee.java
@@ -0,0 +1,8 @@
+public class BlackCoffee implements Coffee {
+    public String getDescription() {
+        return "Black Coffee";
+    }
+    public double getCost() {
+        return 100.00;
+    }
+}
